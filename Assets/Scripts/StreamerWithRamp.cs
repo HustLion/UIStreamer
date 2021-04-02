b1aab863e6fd0e7bd65c43a1ea517ab73e5db312
hustlion <gongjue@ztgame.com>
2021-04-02 17:01:22 +0800

    pos working

--- Assets/Scripts/StreamerWithRamp.cs
+++ Assets/Scripts/StreamerWithRamp.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Coffee.UIEffects
@@ -7,6 +8,8 @@ namespace Coffee.UIEffects
     public class StreamerWithRamp : BaseMaterialEffect
     {
         private const uint k_ShaderId = 1 << 4;
+        private Material mat;
+        
         [Tooltip("Effect Progress")] [SerializeField] [Range(0, 1)]
         float m_Progress = 0f;
 
@@ -87,7 +90,6 @@ namespace Coffee.UIEffects
             // paramTex.RegisterMaterial(newMaterial);
         }
 
-        private Material mat;
         
         public void Play(bool reset = true)
         {
@@ -98,5 +100,58 @@ namespace Coffee.UIEffects
             effectPlayer.Stop(reset);
         }
 
+        private void Update()
+        {
+            if (mat)
+            {
+                var rt = GetComponent<RectTransform>();
+                if (rt)
+                {
+                    var worldCorners = new Vector3[4];
+                    rt.GetWorldCorners(worldCorners);
+                    var bounds = new Bounds(worldCorners[0], Vector3.zero);
+                    for (int i = 0; i < 4; i++)
+                    {
+                        bounds.Encapsulate(worldCorners[i]);
+                    }
+
+                    var min = bounds.min; // LeftBottom
+                    var xyMax = Math.Max(bounds.size.x, bounds.size.y);
+                    var worldLeftTop = new Vector3(min.x, min.y + xyMax, min.z);
+                    var worldRightBottom = new Vector3(min.x + xyMax, min.y, min.z);
+                    var toPosition = new Vector4(worldRightBottom.x, worldRightBottom.y, worldRightBottom.z, 1.0f);
+                    var fromPosition = new Vector4(worldLeftTop.x, worldLeftTop.y, worldLeftTop.z, 1.0f);
+                    // Debug.DrawLine(fromPosition, toPosition, Color.green);
+                    var canvas = GetComponentInParent<Canvas>();
+                    if (canvas.renderMode == RenderMode.WorldSpace)
+                    {
+                        var toPositionSp = Camera.main.WorldToScreenPoint(toPosition);
+                        var fromPositionSp = Camera.main.WorldToScreenPoint(fromPosition);
+
+                        mat.SetVector("_ToPosition", toPositionSp);
+                        mat.SetVector("_FromPosition", fromPositionSp);
+                        mat.SetFloat("_ScreenHeight", 0);
+                        // Debug.Log($"from: {fromPosition}, to: {toPosition}; sp: {fromPositionSp} {toPositionSp}");
+                        
+                    } else if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+                    {
+                        var toPositionSp = toPosition;
+                        var fromPositionSp = fromPosition;
+                        mat.SetVector("_ToPosition", toPositionSp);
+                        mat.SetVector("_FromPosition", fromPositionSp);
+                        mat.SetFloat("_ScreenHeight", Screen.height);
+                        // Debug.Log($"from: {fromPosition}, to: {toPosition}; sp: {fromPositionSp} {toPositionSp}");
+                    } else if (canvas.renderMode == RenderMode.ScreenSpaceCamera) {
+                        var toPositionSp = Camera.main.WorldToScreenPoint(toPosition);
+                        var fromPositionSp = Camera.main.WorldToScreenPoint(fromPosition);
+                        mat.SetVector("_ToPosition", toPositionSp);
+                        mat.SetVector("_FromPosition", fromPositionSp);
+                        mat.SetFloat("_ScreenHeight", 0);
+                        // Debug.Log($"from: {fromPosition}, to: {toPosition}; sp: {fromPositionSp} {toPositionSp}");
+                    }
+                }
+                
+            } 
+        }
     }
 }
